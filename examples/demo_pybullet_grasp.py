52297a17a455b3121d8e601509da82f70e99dab9
JIA XIN <82912588+huchacha1314@users.noreply.github.com>
2024-02-06 11:13:16 +0100

    Update demo_pybullet_grasp.py

--- examples/demo_pybullet_grasp.py
+++ examples/demo_pybullet_grasp.py
@@ -1,4 +1,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+# 请了解一下内容
+# 如何获取tactile sensror的数据
+#    digit=tacto.Sensor
+#    digits.add_camera
+#    digits.add_body
+#    digits.render
+#    digits.updateGUI
+
 
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
